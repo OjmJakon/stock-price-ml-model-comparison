cc1a325a919db80864ec3d98b3c1020c044eb37f
Ziga Petelin <64520596+OjmJakon@users.noreply.github.com>
2025-03-28 21:40:23 +0100

    Create get_data function

--- functions.py
+++ functions.py
@@ -1,3 +1,15 @@
+def get_data(ticker, period):
+    end_data = dict()
+    if isinstance(ticker, list):
+        for i in ticker:
+            ticker = yf.Ticker(i)
+            data = ticker.history(period=period)
+            end_data[i] = data
+    else:
+        data = ticker.history(period=period)
+        end_data[ticker] = data
+    return end_data
+
 def generate_columns(stock):
     stock['Return'] = stock['Close'].pct_change()
     stock['Cum_Return'] = stock['Close'].pct_change().cumsum()
